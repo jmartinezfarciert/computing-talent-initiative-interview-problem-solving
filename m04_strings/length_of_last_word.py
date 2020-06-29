127a004de2eba8329ee4b90ad408ccb4a2a01e03
johnny <me@MacBook-Pro.local>
2020-06-29 12:49:25 -0700

    complete working solution of lenght_of_last word interview bit challabnge question

---
+++ m04_strings/length_of_last_word.py
@@ -0,0 +1,42 @@
+# Given a string s consists of upper/lower-case alphabets and empty space
+# characters ' ', return the length of last word in the string.
+#
+# If the last word does not exist, return 0.
+#
+# Note: A word is defined as a character sequence consists of non-space
+# characters only.
+#
+# Example:
+# Given s = "Hello World",
+# return 5 as length("World") = 5.
+#
+# Please make sure you try to solve this problem without using library
+# functions. Make sure you only traverse the string once.
+# @param A : string
+# @return an integer
+def lengthOfLastWord(A):
+    count = 0
+    A = A.strip()
+
+    if len(A) == 0:
+        return 0
+
+        for i in reversed(A):
+            if i.isalnum():
+                count += 1
+            else:
+                break
+
+                return count
+print(
+length_of_last_word("    ")
+)
+
+# Using Library functions
+def length_of_last_word(words):
+    words = words.strip()
+    words = words.split()
+    if len(words) == 0:
+        return 0
+    last_word = words[len(words)-1]
+    return len(last_word)
