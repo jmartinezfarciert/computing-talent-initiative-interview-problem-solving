3b8dbe84f0b0e7be7473dc8c6691e02a55563e3c
johnny <me@MacBook-Pro.local>
2020-07-07 16:26:51 -0700

    completed merge_sorted_array problem ! Learned that list.insert(index, element) shifts the tail elements of the list! python is amazing !

---
+++ m05_search/merge_sorted_lists.py
@@ -0,0 +1,26 @@
+# Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+#
+# Note:
+# The number of elements initialized in nums1 and nums2 are m and n respectively.
+# You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+# Input:
+nums1 = [1,2,3,0,0,0] #m = 3
+nums2 = [2,5,6]       #n = 3
+#
+# Output: [1,2,2,3,5,6]
+
+def merge_sorted_lists(nums1, nums2):
+    nums1_index = 0
+    nums2_index = 0
+    while nums2_index < len(nums2):
+        if nums2[nums2_index] < nums1[nums1_index] or nums1[nums1_index] == 0:
+            nums1.insert(nums1_index, nums2[nums2_index])
+            nums1.pop()
+            nums2_index += 1
+            nums1_index = nums2_index
+        else :
+            nums1_index += 1
+        print(nums1)
+    return nums1
+
+merge_sorted_lists(nums1, nums2)
