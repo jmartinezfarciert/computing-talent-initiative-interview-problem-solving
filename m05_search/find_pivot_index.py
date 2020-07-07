6afbf8d933ca55c0aeae88ed7d43b4058e2ddf7e
johnny <me@MacBook-Pro.local>
2020-07-07 12:45:18 -0700

    " watch your variables"

--- m05_search/find_pivot_index.py
+++ m05_search/find_pivot_index.py
@@ -16,13 +16,14 @@ def find_pivot_index(input_list):
         test_index = int((end_index - start_index) / 2) + start_index
         if input_list[test_index] < input_list[minimum_index]:
             minimum_index = test_index
-            end = test_index
+            end_index = test_index
         else :
             start_index = test_index
     return minimum_index
-    
+
+# should return 5
 print(
-find_pivot_index([4, 5, 6, 7, 0, 1, 2])
+find_pivot_index([13, 14, 15, 16, 17, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
 )
 
 # Linear time
@@ -32,3 +33,27 @@ def find_pivot_index(input_list):
         # check the current index agains the previous index
         if input_list[i] < min:
             return i
+
+# Liz Howard's solution 
+def find_pivot_index(input_list):
+  # List is sorted, but then rotated.
+  # Find the minimum element in less than linear time
+  # return it's index
+
+  start = 0
+  end = len(input_list) - 1
+  minimum_index = 0
+
+  while start < end-1:
+    if end - start % 2 == 0:
+      pivot = int((end - start) / 2) + start
+    else:
+      pivot = int((end - start + 1) / 2) + start
+
+    if input_list[pivot] < input_list[minimum_index]:
+      end = pivot
+      minimum_index = pivot
+    else:
+      start = pivot
+
+  return minimum_index
